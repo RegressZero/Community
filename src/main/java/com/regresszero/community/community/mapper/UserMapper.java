6407845f194e233d467341523129833e2ee1faa1
regress <1519401929@qq.com>
2020-02-21 08:33:33 +0800

    add check cookies

--- src/main/java/com/regresszero/community/community/mapper/UserMapper.java
+++ src/main/java/com/regresszero/community/community/mapper/UserMapper.java
@@ -3,9 +3,14 @@ package com.regresszero.community.community.mapper;
 import com.regresszero.community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user (NAME,ACCOUNT_ID,TOKEN,GMT_CREATE,GMT_MODIFIED) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
